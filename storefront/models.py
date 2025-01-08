7b0dfa5b2b8097505ef7b96f546272fe7affef21
P1R47E <yosephtesfaye27@gmail.com>
2025-01-08 15:30:35 +0300

    choices_field

--- storefront/models.py
+++ storefront/models.py
@@ -8,9 +8,25 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold')
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.Charfiled(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    
+    membership = models.CharField(max_length=255,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+class Order(models.Model):
+    PAYMENT_STATUS_ChOICE = [
+        ('P','Pending'),
+        ('C','Complete'),
+        ('F','Failed')
+    ]  
+    placed_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(max_length=255,Choices=PAYMENT_STATUS_CHOICES)
